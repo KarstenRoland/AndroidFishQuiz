337089e87536a83cdcae5503fdc8cf93b0b058dc
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2023-04-10 15:42:40 -0500

    Rebase to tabbed project

--- app/src/main/java/com/cs364/fishquiz/MainActivity.kt
+++ app/src/main/java/com/cs364/fishquiz/MainActivity.kt
@@ -1,52 +1,36 @@
 package com.cs364.fishquiz
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.cs364.fishquiz.data.FishDao
-import com.cs364.fishquiz.data.FishRepository
-import com.cs364.fishquiz.ui.fish.FishDBViewModel
-import com.cs364.fishquiz.ui.theme.FishQuizTheme
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.cs364.fishquiz.ui.main.SectionsPagerAdapter
+import com.cs364.fishquiz.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
-    
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            
-            FishQuizTheme {
-                val result by viewModelDB.allFish.collectAsState(initial = emptyList())
 
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FishQuizTheme {
-        Greeting("Android")
     }
 }
