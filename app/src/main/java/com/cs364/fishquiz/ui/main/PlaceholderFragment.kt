15f8d2661194f96656d57ecab7fd9adbd5d86b14
Flare199 <NightFlare199@yahoo.com>
2023-04-21 15:22:28 -0500

    Update PlaceholderFragment.kt

--- app/src/main/java/com/cs364/fishquiz/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/cs364/fishquiz/ui/main/PlaceholderFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
 import com.cs364.fishquiz.R
 import com.cs364.fishquiz.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayout
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -34,14 +38,34 @@ class PlaceholderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val root = binding.root
 
-        val textView: TextView = binding.sectionLabel
-        pageViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val viewPager: ViewPager = binding.viewPager
+        val tabLayout: TabLayout = binding.tabs
+
+        viewPager.adapter = SectionsPagerAdapter(requireContext(), childFragmentManager)
+        viewPager.currentItem = arguments?.getInt(ARG_SECTION_NUMBER) ?: 0
+
+        tabLayout.setupWithViewPager(viewPager)
+
+        // Change the color of the ViewPager background based on the selected tab
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> viewPager.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green))
+                    1 -> viewPager.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.black))
+                    2 -> viewPager.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.teal_200))
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
         })
+
         return root
     }
 
