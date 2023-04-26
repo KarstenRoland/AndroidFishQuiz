30f8af8e0de51245fb77b9377cba237d77febfc2
Flare199 <NightFlare199@yahoo.com>
2023-04-25 23:57:52 -0500

    Revert "Update PlaceholderFragment.kt"
    This reverts commit 21734f73123c48ac89376eeb3ba251356050607f.

--- app/src/main/java/com/cs364/fishquiz/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/cs364/fishquiz/ui/main/PlaceholderFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.cs364.fishquiz.R
@@ -27,7 +29,7 @@ class PlaceholderFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProvider(this)[PageViewModel::class.java].apply {
+        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
             setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
         }
     }
